9337c9cf211bfc4b89d6d53adae95d9bcbab5d94
Shivraj Gore <shivrajgore@Shivrajs-MacBook-Air.local>
2025-04-27 23:07:56 +0530

    added comments

--- Day01_HelloWorld/HelloWorld.java
+++ Day01_HelloWorld/HelloWorld.java
@@ -2,6 +2,7 @@ package Day01_HelloWorld;
 
 public class HelloWorld {
     public static void main(String[] Args){
+        //this is basic java code
         System.out.println("Hello-World");
     }
 }
