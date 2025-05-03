d448817862adc4b75b2017abd978de7bca1c6912
ShivrajGore <shivrajgore@Shivrajs-MacBook-Air.local>
2025-05-03 10:40:06 +0530

    single-D array & for loop

--- Day01_HelloWorld/HelloWorld.java
+++ Day01_HelloWorld/HelloWorld.java
@@ -4,5 +4,16 @@ public class HelloWorld {
     public static void main(String[] Args){
         //this is basic java code
         System.out.println("Hello-World");
+
+        //for loop
+        int[] numbers= {1,2,3,4,5,6};
+        for (int i=0;i < numbers.length;i++) {
+            System.out.println("Element at index" + i + ":" + numbers[i]);
+        }
+
+        //enhanced for loop
+        for (int num : numbers){
+            System.out.println("Elements :" +num);
+        }
     }
 }
